089f017c3e0edf35f77d978f91119b4c37ce21d4
Ashminkhadka <ashminkhadka25@gmail.com>
2024-04-26 08:37:45 +0545

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,48 @@
+export const Footer = () => {
+    return (
+        <>
+            <footer className="padding_4x">
+                <div className="container">
+                    <div className="row ">
+                        <div className="col-sm-6 col-md-6 col-lg-3 padding_1x">
+                            <h5>Navigation</h5>
+                            <a href="#">Home</a>
+                            <a href="#">Add User</a>
+                            <a href="#">About</a>
+                            <a href="#">Contact</a>
+
+                        </div>
+                        <div className="col-sm-6 col-md-6 col-lg-3  padding_1x">
+                            <h5>Information</h5>
+                            <a href="#">Events</a>
+                            <a href="#">Our Team</a>
+                            <a href="#">Upcoming Sale</a>
+                            <a href="#">New Launches</a>
+
+                        </div>
+                        <div className="col-sm-6 col-md-6 col-lg-3  padding_1x">
+                            <h5>Help</h5>
+                            <a href="#">Sign Up</a>
+                            <a href="#">Login</a>
+                            <a href="#">Terms of Services</a>
+                            <a href="#">Privacy Policy</a>
+
+                        </div>
+                        <div className="col-sm-6 col-md-6 col-lg-3  padding_1x">
+                            <h5>Contact Us</h5>
+                            <a href="#">Ekantakuna, Lalitpur, Nepal</a>
+                            <a href="#">ashminkhadka25@gmail.com</a>
+                            <a href="#">+977 9816936802</a>
+
+                        </div>
+                        
+
+                    </div>
+                </div>
+            </footer>
+
+
+        </>
+
+    )
+}
